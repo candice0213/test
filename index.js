5bdc083d717363250ed79e0ef3f5e3fb22211160
linli-kooboo <xlinli0213@outlook.com>
2019-06-21 10:04:09 +0800

    fix

--- index.js
+++ index.js
@@ -3,39 +3,32 @@ const router = require('koa-router')();
 
 const app = new Koa();
 
-router.get('/', (ctx, next) => {
+router.get('/', async (ctx, next) => {
     ctx.response.body = `
     <p>host: ${ctx.request.header.host}</p>
     <p>header: ${JSON.stringify(ctx.request.header)}</p>
     `;
+    await next();
 });
 
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = `
-//     <p>host: ${ctx.request.header.host}</p>
-//     <p>header: ${JSON.stringify(ctx.request.header)}</p>
-//     `;
-//     await next();
-// });
+router.get('/cookie/:name', async (ctx, next) => {
+    const cookieKey = ctx.params.name
+    const cookieValue = ctx.cookies.get(cookieKey)
+    ctx.response.body = `
+    <p>cookie: ${cookieKey} = ${cookieValue}</p>
+    `;
+    await next();
+});
 
-// router.get('/cookie/:name', async (ctx, next) => {
-//     const cookieKey = ctx.params.name
-//     const cookieValue = ctx.cookies.get(cookieKey)
-//     ctx.response.body = `
-//     <p>cookie: ${cookieKey} = ${cookieValue}</p>
-//     `;
-//     await next();
-// });
+router.get('/setcookie', async (ctx, next) => {
+    const cookies = ctx.request.query
+    Object.keys(cookies).map(k => ctx.cookies.set( k, cookies[k], { maxAge: 2*60*60*1000 } ))
 
-// router.get('/setcookie', async (ctx, next) => {
-//     const cookies = ctx.request.query
-//     Object.keys(cookies).map(k => ctx.cookies.set( k, cookies[k], { maxAge: 2*60*60*1000 } ))
-
-//     ctx.response.body = `
-//     <p>已设置cookie: ${JSON.stringify(cookies)}</p>
-//     `;
-//     await next();
-// });
+    ctx.response.body = `
+    <p>已设置cookie: ${JSON.stringify(cookies)}</p>
+    `;
+    await next();
+});
 
 app.use(router.routes());
 
